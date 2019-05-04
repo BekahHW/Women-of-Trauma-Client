7a41b63e6044434cae3ff2e7d6747d8cfde0bfe9
github username <github email address>
2019-05-04 06:32:30 -0400

    Add containers folder and update App.js

---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+import React, { Component} from 'react';
+import './App.css'
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+      <h1>Hello. This is the container.</h1>
+      </div>
+    )
+  }
+}
+  export default App
