8a0737543c7ecf127ab915e9eb608968526ad324
github username <github email address>
2019-05-07 14:04:04 -0400

    Add getDisorders to actions

--- src/actions/disorders.js
+++ src/actions/disorders.js
@@ -1,6 +1,11 @@
 
 const API_URL = process.env.REACT_APP_API_URL;
 
-
-fetch(`${API_URL}/disorders`)
-.then(response => response.json())
+const getDisorders = () => {
+  dispatch => {
+    return fetch(`${API_URL}/disorders`)
+    .then(response => response.json())
+    .then(disorders => dispatch(setDisorders))
+    .catch(error => console.log(error))
+  }
+}
