15e8aa70580c6c9ce3043fa8a49f3ecb106b3e53
github username <github email address>
2019-05-07 05:21:36 -0400

    Extrapolate code from disorder container and put it into disorderCard component

--- src/containers/Disorders.js
+++ src/containers/Disorders.js
@@ -1,14 +1,11 @@
 import React from 'react';
+import DisorderCard from '../components/DisorderCard'
 
 const Disorders = (props) => (
 
       <div className="App-header">
       <h1>Disorders</h1>
-      {props.disorders.map(disorder =>
-        <div>
-        <h2>{disorder.name}</h2>
-        <p>{disorder.description}</p>
-        </div>
+      {props.disorders.map(disorder => < DisorderCard disorder={disorder} />
       )}
       </div>
 )
