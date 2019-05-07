0164ce252b98e1184de7d6ca3470653f4e996366
github username <github email address>
2019-05-07 05:16:13 -0400

    Create surfboard card component

---
+++ src/components/Disorders.js
@@ -0,0 +1,12 @@
+import import React from 'react'
+import PropTypes from 'prop-types'
+import Disorders from './containers/Disorders'
+
+
+const DisorderCard = (disorder) => {
+  return (
+    <div />
+  )
+}
+
+export default DisorderCard
