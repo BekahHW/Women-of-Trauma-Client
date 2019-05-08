93eb620efe79cd7ae2558157ab80e159152b34ca
github username <github email address>
2019-05-08 06:08:39 -0400

    Create form for narrativeForm

--- src/containers/NarrativeForm.js
+++ src/containers/NarrativeForm.js
@@ -1,11 +1,37 @@
 import React, {Component} from 'react'
+import { connect } from 'react-redux'
 
 class NarrativeForm extends Component {
   render () {
+    const {title, tagline, story} = this.props.narrativeFormData
     return (
       <div>
       Add Your Story
       <form>
+        <div>
+          <label htmlForm="title">Title</label>
+          <input
+            type="text"
+            name="title"
+            value={title}
+            />
+          </div>
+          <div>
+            <label htmlForm="tagline">Tagline</label>
+            <input
+              type="text"
+              name="tagline"
+              value={tagline}
+              />
+            </div>
+            <div>
+              <label htmlForm="story">Story</label>
+              <input
+                type="text"
+                name="story"
+                value={story}
+                />
+            </div>
 
       </form>
       </div>
@@ -13,4 +39,10 @@ class NarrativeForm extends Component {
   }
 }
 
-export default NarrativeForm;
+
+const mapStateToProps = (state) => {
+  return ({
+    narrativeFormData: state.narrativeFormData
+  })
+}
+export default connect(mapStateToProps)(NarrativeForm)
