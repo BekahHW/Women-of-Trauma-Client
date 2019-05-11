dac7e7f3ddb66bfcdd01edd4cdd13f22f641e68f
github username <github email address>
2019-05-11 16:01:35 -0400

    Add home component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,42 +1,29 @@
-import React, { Component} from 'react';
+import React from 'react';
 import './App.css'
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 import NavBar from '../components/NavBar'
+import Home from './Home'
 import Disorders from './Disorders'
 import Narratives from './Narratives'
 import getHelp from '../components/GetHelp'
 import Welcome from '../components/Welcome'
 
-
-
-
-
-class App extends Component {
-
-  componentDidMount() {
-
-  }
-
-  render() {
-    return (
+const App = () => (
       <div className="App">
 
       <Router>
       <React.Fragment>
         <NavBar />
+          <Route exact path="/" component={Home} />
+
         <Route exact path="/disorders" component={Disorders} />
         <Route exact path="/narratives" component={Narratives} />
         <Route exact path="/getHelp" component={getHelp} />
 
         </React.Fragment>
         </Router>
-        <Welcome />
-      <h1>Hello. This is the container.</h1>
-
 
       </div>
     )
-  }
-}
 
-  export default App
+export default App
---
+++ src/containers/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Welcome from '../components/Welcome'
+
+const Home = () => (
+  <div>
+    <Welcome />
+    <h1>Hello. This is the container.</h1>
+  </div>
+)
+
+
+  export default Home
