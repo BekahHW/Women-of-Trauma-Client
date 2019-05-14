a40ad7a868b2519221a6e530571b5ab31812abdd
github username <github email address>
2019-05-14 05:55:34 -0400

    Add banner component and load to home

---
+++ src/components/Banner.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import photo from '../images/photo.jpg'
+import '../containers/App.css'
+
+
+
+
+
+const Banner = () => (
+    <div className="banner"
+    style={{ width: "100%", height: "30%", margin: "auto", padding: "50px" }}>
+    <img src={photo} alt="Girl in a jacket. Side profile in darkness" />
+    <p>You don't have to face your trauma alone.</p>
+    </div>
+
+  )
+
+
+export default Banner
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import Welcome from '../components/Welcome'
+import Banner from '../components/Banner'
+
 
 const Home = () => (
   <div>
+    <Banner />
     <Welcome />
     <h1>Hello. This is the container.</h1>
   </div>
