6c9cbe765368b24cf59a08032ca80947e681f7b6
github username <github email address>
2019-05-14 06:00:04 -0400

    Add image credit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -8,8 +8,10 @@ import '../containers/App.css'
 
 const Banner = () => (
     <div className="banner"
-    style={{ width: "100%", height: "30%", margin: "auto", padding: "50px" }}>
-    <img src={photo} alt="Girl in a jacket. Side profile in darkness" />
+    style={{ width: "100%", height: "30%", margin: "auto", padding: "50px" }}
+    >
+    <img src={photo} alt="Girl in a jacket. Side profile in darkness; Credit: Photo by Fernando Brasil on Unsplash" />
+
     <p>You don't have to face your trauma alone.</p>
     </div>
 
