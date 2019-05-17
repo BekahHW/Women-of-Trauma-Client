32463f4286719f135c608b66eced05d0c28b511c
github username <github email address>
2019-05-17 09:33:59 -0400

    Eliminate redundant photo in image description

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -10,7 +10,7 @@ const Banner = () => (
     <div className="banner"
     style={{ width: "100%"}}
     >
-    <img src={photo} alt="Girl in a jacket. Side profile in darkness; Credit: Photo by Fernando Brasil on Unsplash" height= "200" />
+    <img src={photo} alt="Girl in a jacket. Side profile in darkness; Credit: Fernando Brasil on Unsplash" height= "200" />
 
     <p>You don't have to face your trauma alone.</p>
     </div>
