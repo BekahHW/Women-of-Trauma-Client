7f414c7879a5c2b3d46f2dd097d6bbe3bbf99bf1
github username <github email address>
2019-06-17 14:00:29 -0400

    Add Main Quote component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Welcome from '../components/Welcome'
 import Banner from '../components/Banner'
+import MainQuote from '../components/MainQuote'
+
 
 
 const Home = () => (
---
+++ src/components/MainQuote.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const pStyle = {
+  fontSize: '2em',
+  textAlign: 'center',
+  color: 'white',
+  background: 'blue'
+};
+
+const MainQuote = () => (
+ 
+
+  <p style={pStyle}>“Traumatic events, by definition, overwhelm our ability to cope.When the mind becomes flooded with emotion, a circuit breaker is thrown that allows us to survive the experience fairly intact, that is, without becoming psychotic or frying out one of the brain centers.The cost of this blown circuit is emotion frozen within the body.In other words, we often unconsciously stop feeling our trauma partway into it, like a movie that is still going after the sound has been turned off.We cannot heal until we move fully through that trauma, including all the feelings of the event.”
+― Susan Pease Banitt, The Trauma Tool Kit: Healing PTSD from the Inside Out</p>
+
+)
+
+export default MainQuote
